37e98789b77302857617e990cabb6b0f5d4e7a9a
shadowthebat <10837037+shadowthebat@users.noreply.github.com>
2019-02-06 18:15:19 -0500

    initial commit

---
+++ pip--upgrade_all.py
@@ -0,0 +1,31 @@
+import os
+'''
+First create txt file of the result of calling pip list
+Instructions:
+    $ pip list > piplist.txt
+
+'''
+# Read the file
+with open('piplist.txt', 'r+') as f:
+    pipl = f.read()
+
+pipl = pipl.split()  # convert to list in preparating to isolate packages from original string
+# To find starting index
+for i in pipl:
+    if i == 'alabaster':
+        start = pipl.index(i)
+
+# Strips everything from list until first package
+pipl = pipl[start:]
+
+# Strips away version numbers, And leaves us with desired list of packages
+piplist = [pipl[x] for x in range(len(pipl)-1) if x % 2 == 0]
+
+
+def upgrade():
+    # THE MAGIC: upgrades all packages
+    for i in pipl[start:end]:
+        os.system(f'pip install --upgrade {i}')
+
+
+upgrade()
