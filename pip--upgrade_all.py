99b8dc52d0dd539e767f8ab4c3591e6a84d493ff
shadowthebat <10837037+shadowthebat@users.noreply.github.com>
2019-02-06 18:53:45 -0500

    tidy up

--- pip--upgrade_all.py
+++ pip--upgrade_all.py
@@ -1,26 +1,23 @@
 import os
 
-# Creates text file of the result of calling pip list
-os.system(f'pip list > piplist.txt')
-
-# Read the file
-with open('piplist.txt', 'r+') as f:
-    pipl = f.read()
-
-pipl = pipl.split()  # convert to list in preparating to isolate packages from original string
-# To find starting index
-for i in pipl:
-    if i == 'alabaster':
-        start = pipl.index(i)
-
-# Strips everything from list until first package
-pipl = pipl[start:]
-
-# Strips away version numbers, And leaves us with desired list of packages
-pipl = [pipl[x] for x in range(len(pipl)-1) if x % 2 == 0]
-
 
 def upgrade():
+    # Creates text file of the result of calling pip list
+    os.system(f'pip list > piplist.txt')
+
+    # Read the file
+    with open('piplist.txt', 'r+') as f:
+        pipl = f.read()
+
+    # convert to list in preparating to isolate packages from original string
+    pipl = pipl.split()
+
+    # Strips everything from list until first package
+    pipl = pipl[4:]
+
+    # Strips away version numbers, And leaves us with desired list of packages
+    pipl = [pipl[x] for x in range(len(pipl)-1) if x % 2 == 0]
+
     # THE MAGIC: upgrades all packages
     for i in pipl:
         os.system(f'pip install --upgrade {i}')
